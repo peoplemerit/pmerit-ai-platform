1c2e0464a20b50b1280616879eb04663edce47fa
Idowu Gabriel <peoplemerit@gmail.com>
2025-09-10 22:51:45 -0400

    initial boot

--- assets/boot-includes.js
+++ assets/boot-includes.js
@@ -1,1 +1,13 @@
+async function loadIncludes() {
+  const header = await fetch('/partials/header.html').then(res => res.text());
+  const footer = await fetch('/partials/footer.html').then(res => res.text());
 
+  document.getElementById('header-container').innerHTML = header;
+  document.getElementById('footer-container').innerHTML = footer;
+
+  // Signal that header/footer are ready
+  window.dispatchEvent(new Event('pmerit:initialized'));
+}
+
+// Load includes immediately
+loadIncludes();
