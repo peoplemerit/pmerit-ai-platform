058c42bacdf75a54fc3eef32c70b65302fa34f35
Idowu Gabriel <peoplemerit@gmail.com>
2025-09-12 20:11:23 -0400

    Load header, body, and footer on DOMContentLoaded

--- assets/boot-includes.js
+++ assets/boot-includes.js
@@ -38,3 +38,16 @@ fetch('/partials/body.html')
 fetch('/partials/footer.html')
   .then(res => res.text())
   .then(html => document.getElementById('footerContainer').innerHTML = html);
+document.addEventListener("DOMContentLoaded", () => {
+  fetch('/partials/header.html')
+    .then(res => res.text())
+    .then(html => document.getElementById('headerContainer').innerHTML = html);
+
+  fetch('/partials/body.html')
+    .then(res => res.text())
+    .then(html => document.getElementById('bodyContainer').innerHTML = html);
+
+  fetch('/partials/footer.html')
+    .then(res => res.text())
+    .then(html => document.getElementById('footerContainer').innerHTML = html);
+});
